fce1f52b20460f989b91f90c84872d79976aecf7
Etienne <etienne.micheladj@gmail.com>
2024-09-17 16:54:53 +0200

    minor(lab_api): simplify core and adding instruction manager

---
+++ lab_api/lab_api/instruction_manager.py
@@ -0,0 +1,41 @@
+import pandas as pd
+
+class InstructionManager():
+    def __init__(self, database):
+        self.database = database
+
+    def display_instructions(self, instruction_table):
+        return self.database.getTable(instruction_table)
+        
+    def is_instruction_exist(self, instruction_table, instruction):
+        instruction.pop("id", None)
+
+        instruction_already_exist = False
+        instruction_id = None
+        instructions_inside_db = self.database.getTable(instruction_table)
+        instructions_inside_db = instructions_inside_db.to_dict(orient='records')
+        for existing_instruction in instructions_inside_db:
+            id = existing_instruction.pop("id", None)
+            if instruction == existing_instruction:
+                instruction_already_exist = True
+                instruction_id = id
+        if instruction_id is None:
+            instruction_id = len(instructions_inside_db) + 1
+        return instruction_already_exist, instruction_id
+        
+    def add_instruction(self, instruction_table, instruction):
+        instruction_already_exist,instruction_id = self.is_instruction_exist(instruction_table, instruction)
+        instruction["id"] = instruction_id
+        if not instruction_already_exist:
+            self.database.append_to_table(instruction_table, pd.DataFrame([instruction]))
+            return "Success"
+        return "Already exist"
+
+    def remove_instruction(self, instruction_table, instruction):
+        instruction_already_exist, instruction_id = self.is_instruction_exist(instruction_table, instruction)
+        if instruction_already_exist:
+            instructions_inside_db = self.database.getTable(instruction_table)
+            instructions_inside_db.drop(index=instructions_inside_db[instructions_inside_db["id"] == instruction_id].index, inplace=True)
+            self.database.replace_table(instruction_table,instructions_inside_db)
+            return "Success"
+        return "Instruction not exist"
