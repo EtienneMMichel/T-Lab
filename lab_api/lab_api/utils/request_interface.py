d96f92f6b21d659a87eed297cb382078694adf1d
Etienne <etienne.micheladj@gmail.com>
2024-09-17 18:56:53 +0200

    fix(lab_api): add request interface

--- lab_api/lab_api/utils/request_interface.py
+++ lab_api/lab_api/utils/request_interface.py
@@ -4,8 +4,12 @@ class InstructionBody(BaseModel):
     table:str
     instruction: dict
 
+class DisplayBody(BaseModel):
+    table:str
+
 class CollectorInstruction:
     exchange:int
     symbol:str
     rate:int
     data_type:str
+
